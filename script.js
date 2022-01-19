7c7661150e3f910cbceed57a1dd6066a3ea03423
bperez05 <perez.brandon55@yahoo.com>
2022-01-19 14:49:03 -0600

    loads HTML and CSS

--- script.js
+++ script.js
@@ -1,13 +1,4 @@
-// fuction loacds html and css files
-$(document).ready(function () {
-
-    //pulled from moment.js to display date and time
-    $("#currentDay").text(moment().format("MMM Do YYYY, h:mm:ss a"));
-
-    $(".saveBtn").on("click", function () { 
-        console.log(this);
-        var text = $(this).siblings(".description").val(); 
-        var time = $(this).parent().attr("id");
-
-        localStorage.setItem(time, text);
-})
+$(document).ready(function () { //loads html then css
+    //shows current date and time
+    $("#currentDay").text(moment().format("MMMM Do YYYY, h:mm:ss a")); // pulled format from moment.js 
+  
